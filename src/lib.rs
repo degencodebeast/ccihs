d4a12261575dffff606949675343fa8817ed660f
degencodebeast <degencodebeast@protonmail.com>
2024-07-25 14:09:55 +0100

    feat: setup structure for ccihs

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+pub mod core;
+pub mod protocol;
+pub mod api;
+pub mod hook;
+pub mod utility;
+
+// pub fn add(left: usize, right: usize) -> usize {
+//     left + right
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
